bad7b6abd9c8883e596aee2164b471c035505578
kefeh <kefehcollins@gmail.com>
2019-11-21 19:42:04 +0100

    Type: Feature, Body: Adds code for the update of a news item

--- application/news/news.py
+++ application/news/news.py
@@ -7,7 +7,7 @@ from .utils import save_news, get_newses
 
 @news_bp.route('', methods=["POST"])
 def add_news():
-    """ This function is used to get the request for adding Carousels to the database
+    """ This function is used to get the request for adding news to the database
     """
     data = request.args
     description = data.get('description')
@@ -15,24 +15,24 @@ def add_news():
     rank = data.get('rank')
 
     if (not title) or (not description) or (not rank):
-        return jsonify(result={'failed_msg': "Unanle to save carousel with missing fields"})
+        return jsonify(result={'failed_msg': "Unanle to save news with missing fields"})
 
     return jsonify(result=save_news(title, description, rank))
 
 
-@carousel_bp.route('', methods=['PUT'])
-def update_carousels():
-    """ This function is used to update any existing carousel """
+@news_bp.route('', methods=["POST"])
+def update_news():
+    """ This function is used to get the request for adding news to the database
+    """
     data = request.args
     description = data.get('description')
-    image = data.get('image')
+    image = data.get('title')
     rank = data.get('rank')
-    carousel_id = data.get('id')
 
-    if (not image) or (not description) or (not rank) or (not carousel_id):
-        return jsonify(result={'failed_msg': "Unable to update carousel with missing fields"})
+    if (not title) or (not description) or (not rank):
+        return jsonify(result={'failed_msg': "Unanle to save news with missing fields"})
 
-    return jsonify(result=save_carousel(image, description, rank, _id=carousel_id))
+    return jsonify(result=save_news(title, description, rank))
 
 
 @carousel_bp.route('', methods=['GET'])
