8c0f26c3e94e45fc8f1d56853a42a6d239d745d8
kefeh <kefehcollins@gmail.com>
2019-11-23 20:11:02 +0100

    Type: Remodel, Body: Remodels the code to return only the added or updated carousel when a POST or PUT request is made

--- application/carousel/utils.py
+++ application/carousel/utils.py
@@ -1,28 +1,54 @@
 from bson import ObjectId
 
 
-def save_carousel(image, description, rank, _id=None):
+def save_carousel(image, description, rank, _id=None, count=0):
     from models import db
     rank = int(rank)
+    value = update_ranks(rank)
+
     a_carousel = db.Carousel() if not _id else db.Carousel.find_one({'_id': ObjectId(_id)})
-    some_carousel = db.Carousel.find_one({'rank': rank})
-    a_carousel['description'] = description
-    a_carousel['image'] = image
-    a_carousel['rank'] = rank
-    try:
-        a_carousel.save()
-    except Exception as exp:
-        print(exp)
-        return {}
-    if some_carousel:
-        save_carousel(some_carousel['image'], some_carousel['description'], some_carousel['rank'] + 1, some_carousel['_id'])
+    if value:
+        a_carousel['description'] = description
+        a_carousel['image'] = image
+        a_carousel['rank'] = rank
+        try:
+            a_carousel.save()
+        except Exception as exp:
+            raise
+            return {'failed_msg': "Database issues, unable to save Carousel, contact admin"}
+    else:
+        return {'failed_msg': "Database issues, unable to save Carousel, contact admin"}
 
-    return get_carousels()
+    return get_carousels(cond=dict(a_carousel))
 
 
-def get_carousels(maximum=None):
+def update_ranks(rank):
+
     from models import db
-    carousels = list(db.Carousel.find({})) if not maximum else list(db.Carousel.find({'rank': {'$lte': int(maximum)}}))
+    rank = int(rank)
+    value = True
+    while rank:
+        some_carousel = db.Carousel.find_one({'rank': rank})
+        if not some_carousel:
+            value = False
+            break
+        rank += 1
+        some_carousel['rank'] = rank
+        try:
+            some_carousel.save()
+        except Exception as exp:
+            raise
+            value = False
+            break
+
+    return True
+
+
+def get_carousels(maximum=None, cond={}):
+    from models import db
+    if maximum:
+        cond = {'rank': {'$lte': int(maximum)}}
+    carousels = list(db.Carousel.find({})) if not cond else list(db.Carousel.find(cond))
     for carousel in carousels:
         carousel['_id'] = str(carousel['_id'])
-    return carousels
+    return carousels if (maximum or not cond) else carousels[0]
