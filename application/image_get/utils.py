4112d85a2f2602bd27c6ae0919746db6c8acff5f
kefeh <kefehcollins@gmail.com>
2019-12-08 13:10:25 +0100

    Type: Feature, Body: Adds the utility functions

---
+++ application/image_get/utils.py
@@ -0,0 +1,17 @@
+from bson import ObjectId
+from application.utils.utils import image_decode_save
+
+def save_image(image, name):
+    return image_decode_save(image, name, 'others')
+
+def get_images():
+    import os
+    from m_dir import BASE_IMAGE_PATH
+
+    new_path = os.path.join(BASE_IMAGE_PATH, 'others')
+    if not os.path.exists(new_path):
+        return []
+
+    files_path = [os.path.abspath(x) for x in os.listdir(new_path)]
+    
+    return files_path
