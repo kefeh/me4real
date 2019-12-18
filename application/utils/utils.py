4b08e88ab6b47d5cd61890c8b51a1398fa011295
kefeh <kefehcollins@gmail.com>
2019-12-18 14:01:59 +0100

    Type: Feature, Body: implements usage of s3 for image save and fetch

--- application/utils/utils.py
+++ application/utils/utils.py
@@ -1,4 +1,8 @@
 import base64
+import boto3
+
+
+BASE_URl = "http://me4real-storage.s3.amazonaws.com"
 
 
 def image_decode_save(image, image_name, category):
@@ -7,6 +11,8 @@ def image_decode_save(image, image_name, category):
         then saves the image in the images folder and returns the appropriate path name for that
         image
     """
+
+    bucket = 'me4real-storage'
 
     image_info = image.split(';')[0].split(':')[-1]
     some_image = image.split(';')[1].split('base64,')[-1] + '=='
@@ -25,4 +31,35 @@ def image_decode_save(image, image_name, category):
     with open(image_path, 'wb') as image_file:
         image_file.write(base64.decodebytes(some_image.encode()))
 
-    return image_path
+    response = upload_file(image_path, bucket)
+    # content = list_files(bucket)
+    print(response)
+
+    return response
+
+
+def upload_file(file_name, bucket):
+    """
+    Function to upload a file to an S3 bucket
+    """
+    object_name = file_name.split('/')[-1]
+    s3_client = boto3.client('s3')
+    with open(file_name, "rb") as f:
+        response = s3_client.upload_fileobj(f, bucket, object_name, ExtraArgs={'ContentType': "image/jpeg"})
+
+
+    response = f'{BASE_URl}/{object_name}'
+
+    return response
+
+
+def list_files(bucket):
+    """
+    Function to list files in a given S3 bucket
+    """
+    s3 = boto3.client('s3')
+    contents = []
+    for item in s3.list_objects(Bucket=bucket)['Contents']:
+        contents.append(item)
+
+    return contents
