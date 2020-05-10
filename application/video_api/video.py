c1d699603a50428dbf0f2f8f08d278975d9b253e
kefeh <kefehcollins@gmail.com>
2020-05-10 07:42:13 +0100

    undoing changes to video api

--- application/video_api/video.py
+++ application/video_api/video.py
@@ -3,24 +3,24 @@ from flask import Blueprint, request, jsonify
 
 video_bp = Blueprint('video', __name__, url_prefix='/api/video/')
 
-# from .utils import video_decode_save
+from .utils import video_decode_save
 
-# @video_bp.route('', methods=["POST"])
-# def add_video():
-#     """ This function is used to get the request for adding video to the database
-#     """
-#     data = request.get_json()
-#     description = data.get('description', '')
-#     image = data.get('video')
-#     title = data.get('title')
+@video_bp.route('', methods=["POST"])
+def add_video():
+    """ This function is used to get the request for adding video to the database
+    """
+    data = request.get_json()
+    description = data.get('description', '')
+    image = data.get('video')
+    title = data.get('title')
 
-#     if (not title) or (not video) or (not rank):
-#         return jsonify(result={'error': "Unanle to save video with missing fields"}), 400
+    if (not title) or (not video) or (not rank):
+        return jsonify(result={'error': "Unanle to save video with missing fields"}), 400
 
-#     resp = video_decode_save(video, title, description)
-#     if 'error' in response:
-#         status_code = 400
-#     else: 
-#         status_code = 200
+    resp = video_decode_save(video, title, description)
+    if 'error' in response:
+        status_code = 400
+    else: 
+        status_code = 200
 
-#     return jsonify(result=resp), status_code
+    return jsonify(result=resp), status_code
